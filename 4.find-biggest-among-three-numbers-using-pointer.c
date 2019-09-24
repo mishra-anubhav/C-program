b416dae3fe7120b818f175865fb52dd8840d5dbf
anubhavmishra1 <39110231+anubhavmishra1@users.noreply.github.com>
2019-09-24 22:46:13 +0530

    Edited

--- 4.find-biggest-among-three-numbers-using-pointer.c
+++ 4.find-biggest-among-three-numbers-using-pointer.c
@@ -2,9 +2,14 @@
 
 int main ()
 {
-
-   char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
-   printf("Greeting message: %s\n", greeting );
+int a=12,b=3,c=45;
+int *p=a,*q=b,*r=c;
+if( *p>*q && *p>*r)
+printf("biggest of all is %d with value %d",*p,p);
+if( *q>*p && *q>*r)
+printf("biggest of all is %d with value %d",*p,p);
+else
+printf("biggest of all is %d with value %d",*r,r);
 }
 /*TESTING
 On the way */
