9e6fd2eba89c99c6ce37e49073e29f8a4c2603ed
anubhavmishra1 <39110231+anubhavmishra1@users.noreply.github.com>
2019-09-26 22:12:29 +0530

    Edited

---
+++ 6.to-sort-a-list-of-string-words-using-an-array-of-pointers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+   char array[5];
+   int i,sum=0;
+   char *ptr;
+
+   printf("\nEnter array elements (5 strings):");
+   for(i=0;i<5;i++)
+      scanf("%s",&array[i]);
+
+   /* array is equal to base address
+    * array = &array[0] */
+   ptr = array;
+
+   for(i=0;i<5;i++){
+        printf("%s",array[i]);
+   }
+   printf("\nThe sum is: %d",sum);
+}
