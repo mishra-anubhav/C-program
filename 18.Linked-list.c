73f3ea412545daac8c7e12edf3dd06174fd865e4
anubhavmishra1 <39110231+anubhavmishra1@users.noreply.github.com>
2019-11-03 23:43:10 +0530

    Added-basic-program

--- 18.Linked-list.c
+++ 18.Linked-list.c
@@ -1,113 +1,89 @@
-#include<stdio.h>
-#include<conio.h>
-struct node
-{
-      int info;
-      struct node *link;
-};
-struct node *FIRST;
-void createlist()
-{
-      char ch;
-      printf("Enter n for break\n");
-      ch=getchar();
-      while(ch!='n')
-      {
-      struct node *NEW_NODE,*SAVE;int x;
-      NEW_NODE=(struct node *)malloc(sizeof(struct node));
-      printf("Enter Data:");
-      scanf("%d",&x);
-      NEW_NODE->info=x;
-      if(FIRST==NULL)
-      {
-         NEW_NODE->link=NULL;
-         FIRST=NEW_NODE;
-      }
-      else
-      {
-         SAVE=FIRST;
-         while(SAVE->link!=NULL)
-         {
-            SAVE=SAVE->link;
-         }
-         SAVE->link=NEW_NODE;
-         NEW_NODE->link=NULL;
-      }
-      fflush(stdin);
-      printf("Enter n for break\n");
-      ch=getchar();
+// A complete working C program to delete a node in a linked list 
+// at a given position 
+#include <stdio.h> 
+#include <stdlib.h> 
+
+// A linked list node 
+struct Node 
+{ 
+	int data; 
+	struct Node *next; 
+}; 
+
+/* Given a reference (pointer to pointer) to the head of a list 
+and an int, inserts a new node on the front of the list. */
+void push(struct Node** head_ref, int new_data) 
+{ 
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+	new_node->data = new_data; 
+	new_node->next = (*head_ref); 
+	(*head_ref) = new_node; 
+} 
+
+/* Given a reference (pointer to pointer) to the head of a list 
+and a position, deletes the node at the given position */
+void deleteNode(struct Node **head_ref, int position) 
+{ 
+// If linked list is empty 
+if (*head_ref == NULL) 
+	return; 
+
+// Store head node 
+struct Node* temp = *head_ref; 
+
+	// If head needs to be removed 
+	if (position == 0) 
+	{ 
+		*head_ref = temp->next; // Change head 
+		free(temp);			 // free old head 
+		return; 
+	} 
+int i=0; 
+	// Find previous node of the node to be deleted 
+	for (temp!=NULL && i<position-1; i++;){ 
+		temp = temp->next; 
    }
-}
-void insertbefore(int x)
-{
-      struct node *NEW_NODE,*SAVE,*PRED;
-      int value;
-      NEW_NODE=(struct node *)malloc(sizeof(struct node));
-      printf("Enter value of node:");
-      scanf("%d",&value);
-      NEW_NODE->info=value;
-      if(FIRST==NULL)
-      {
-         printf("Specified Node Not Found\n");
-      }
-      else
-      {
-          SAVE=FIRST;
-          while(x!=SAVE->info && SAVE->link!=NULL)
-          {
-          PRED=SAVE;
-          SAVE=SAVE->link;
-          }
-          if(SAVE->info==x)
-          {
-          if(FIRST==SAVE)
-          {
-              NEW_NODE->link=SAVE;
-              FIRST=NEW_NODE;
-          }
-          else
-          {
-              NEW_NODE->link=SAVE;
-              PRED->link=NEW_NODE;
-          }
-          }
-          else
-          {
-          printf("Specified Node Not Found");
-          }
-      }
-}
-void display()
-{
-      struct node *SAVE;
-      if(FIRST==NULL)
-      {
-         printf("Linked List is empty\n");
-         return;
-      }
-      printf("elements are:\n");
-      SAVE=FIRST;
-      while(SAVE!=NULL)
-      {
-         if(SAVE->link==NULL)
-         printf("|%d|",SAVE->info);
-         else
-         printf("|%d|->",SAVE->info);
-         SAVE=SAVE->link;
-      }
-      printf("\n");
-      return;
-}
-void main()
-{
-      int x;
-      clrscr();
-      FIRST=NULL;
-      createlist();
-      display();
-      printf("Enter value of Node before which you want to enter new node:");
-      scanf("%d",&x);
-      insertbefore(x);
-      display();
-      getch();
-}
+	// If position is more than number of ndoes 
+	if (temp == NULL || temp->next == NULL) 
+		return; 
+
+	// Node temp->next is the node to be deleted 
+	// Store pointer to the next of node to be deleted 
+	struct Node *next = temp->next->next; 
+
+	// Unlink the node from linked list 
+	free(temp->next); // Free memory 
+
+	temp->next = next; // Unlink the deleted node from list 
+} 
+
+// This function prints contents of linked list starting from 
+// the given node 
+void printList(struct Node *node) 
+{ 
+	while (node != NULL) 
+	{ 
+		printf(" %d ", node->data); 
+		node = node->next; 
+	} 
+} 
+
+/* Drier program to test above functions*/
+int main() 
+{ 
+	/* Start with the empty list */
+	struct Node* head = NULL; 
+
+	push(&head, 7); 
+	push(&head, 1); 
+	push(&head, 3); 
+	push(&head, 2); 
+	push(&head, 8); 
+
+	puts("Created Linked List: "); 
+	printList(head); 
+	deleteNode(&head, 4); 
+	puts("\nLinked List after Deletion at position 4: "); 
+	printList(head); 
+	return 0; 
+} 
